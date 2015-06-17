c28cbe52d1e572a77377a8a8cfb6bf7e370748ae
sarumain <sandeeparumain@gmail.com>
2015-06-17 18:56:50 -0400

    Moving pieces

--- src/checkers/Board.java
+++ src/checkers/Board.java
@@ -14,17 +14,27 @@ public class Board {
 			for (int x = 0; x < 8; x++) {
 				if (x % 2 == y % 2) {
 					if (y <= 2) {
-						pieces[y][x] = new Piece(true);
+						pieces[y][x] = new Piece(true); // creates a red checkers piece
 					} else if (y >= 5) {
-						pieces[y][x] = new Piece(false);
+						pieces[y][x] = new Piece(false); // creates a black checkers piece
 					} else {
-						pieces[y][x] = null;
+						pieces[y][x] = null; // no piece here
 					}
 				} else {
-					pieces[y][x] = null;
+					pieces[y][x] = null; // no piece here
 				}
 			}
 		}
+		
+	}
+	
+	public void movePiece(int y, int x, Piece pieces[][]) {
+		if (isMoveLegal(y,x,pieces) == true) {
+		}
+	}
+	
+	public boolean isMoveLegal(int y, int x, Piece pieces[][]) {
+		return true;
 	}
 
 }
