fb78e77a6af72b3be74fae224b6db44c57bd9d79
sarumain <sandeeparumain@gmail.com>
2015-06-17 16:51:07 -0400

    Changing pieces

--- src/checkers/Board.java
+++ src/checkers/Board.java
@@ -1,5 +1,19 @@
 package checkers;
 
 public class Board {
+	
+	Board() {
+		Piece[][] pieces = new Piece[8][8]; // creates 8x8 array, of which the entries are either NULL or represent a checkers piece
+	}
+	
+	public void specifiyPieces() {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (i % 2 == j % 2) {
+					pieces[i][j] = 
+				}
+			}
+		}
+	}
 
 }
--- src/checkers/Piece.java
+++ src/checkers/Piece.java
@@ -2,10 +2,23 @@ package checkers;
 
 public class Piece {
 	
-	boolean isPieceRed;
+	private boolean isPieceRed;
 	int X;
 	int Y;
 	
-	
+	public Piece() {
+		this.setPieceRed(isPieceRed);
+		this.X = X;
+		this.Y = Y;
+	}
 
+	boolean isPieceRed() {
+		return isPieceRed;
+	}
+
+	void setPieceRed(boolean isPieceRed) {
+		this.isPieceRed = isPieceRed;
+	}
+	
+	
 }
