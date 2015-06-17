9202b14aa905823e4d7942ef828a8d00a084a27f
sarumain <sandeeparumain@gmail.com>
2015-06-17 17:37:44 -0400

    Edit

--- src/checkers/Board.java
+++ src/checkers/Board.java
@@ -13,14 +13,14 @@ public class Board {
 			for (int x = 0; x < 8; x++) {
 				if (x % 2 == y % 2) {
 					if (y <= 2) {
-						
+						pieces[y][x].isPieceRed = true;
 					} else if (y >= 5) {
-						
+						pieces[y][x].isPieceRed = false;
 					} else {
-						// null
+						pieces[y][x] = null;
 					}
 				} else {
-					// null
+					pieces[y][x] = null;
 				}
 			}
 		}
